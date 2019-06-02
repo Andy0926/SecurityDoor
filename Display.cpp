4ba6b792cd454ec283a680bd16fab01a86a51fef
Tan Andy <tanandy2011@hotmail.com>
2019-06-02 15:58:37 +0800

    Added Other Functions

--- Display.cpp
+++ Display.cpp
@@ -3,16 +3,15 @@
 #include "LiquidCrystal_PCF8574.h"
 LiquidCrystal_PCF8574 lcd(0x27);
 
-Display::Display(int relayPin, int buttonPin, int ledPin, int redPin, int buzzerPin)
+Display::Display(int relayPin, int ledPin, int redPin, int buzzerPin)
 {
+  
   pinMode(relayPin, OUTPUT);
-  pinMode(buttonPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(redPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
-  _buttonPin=buttonPin;
-  _relayPin=relayPin;
+  _relayPin = relayPin;
   _ledPin = ledPin;
   _redPin = redPin;
   _buzzerPin = buzzerPin;
@@ -48,12 +47,14 @@ void Display::open()
 
 void Display::lcdDenied()
 {
+  lcd.setBacklight(1);
   lcd.begin(16, 2);
   lcd.print("Access Denied");
 }
 
 void Display::lcdAuthorised()
 {
+  lcd.setBacklight(1);
   lcd.begin(16, 2);
   lcd.print("Access Granted");
 }
@@ -61,9 +62,92 @@ void Display::lcdAuthorised()
 void Display::lcdStart()
 {
   // set up the LCD's number of columns and rows:
+  lcd.setBacklight(1);
   lcd.begin(16, 2);
   lcd.print("   Welcome to");
   lcd.setCursor(0, 1);
   //Print a message to second line of LCD
   lcd.print("    Incubator");
+}
+
+void Display::lcdPassword()
+{
+  lcd.setBacklight(1);
+  lcd.begin(16, 2);
+  lcd.print("Enter Password");
+  lcd.setCursor(0, 1);
+  lcd.print("Press * to Enter");
+}
+
+void Display::printPassword(String pw)
+{
+  lcd.setBacklight(1);
+  lcd.begin(16, 2);
+  lcd.print("Enter Password");
+  lcd.setCursor(0, 1);
+  lcd.print(pw);
+}
+
+void Display::lcdFpAccess(int fingerID)
+{
+  lcd.setBacklight(1);
+  switch (fingerID)
+  {
+  case 1:
+    lcd.begin(16, 2);
+    lcd.print("Welcome Andy");
+    break;
+  case 2:
+    lcd.begin(16, 2);
+    lcd.print("Welcome ChiaLing");
+
+    break;
+  case 3:
+    lcd.begin(16, 2);
+    lcd.print("Welcome Boonie");
+
+    break;
+  case 4:
+    lcd.begin(16, 2);
+    lcd.print("Welcome Li En");
+
+    break;
+  case 5:
+    lcd.begin(16, 2);
+    lcd.print("Welcome Kevin");
+
+    break;
+  case 6:
+    lcd.begin(16, 2);
+    lcd.print("Welcome Jeff");
+
+    break;
+  default:
+    break;
+  }
+}
+
+void Display::fpNewID(String sFingerID)
+{
+  lcd.setBacklight(1);
+  lcd.begin(16, 2);
+  lcd.print("Fingerprint ID");
+  lcd.setCursor(0, 1);
+  lcd.print(sFingerID);
+}
+
+void Display::lcdInvalidID()
+{
+  lcd.setBacklight(1);
+  lcd.begin(16, 2);
+  lcd.print("Invalid ID");
+  lcd.setCursor(0, 1);
+  close();
+}
+
+void Display::lcdFpStored(){
+  lcd.setBacklight(1);
+  lcd.begin(16, 2);
+        lcd.print("FingerprintStored");
+        buzzer();
 }
