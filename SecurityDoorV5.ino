16b69e647e0ab3a80fb445adaf458dbd01243b63
Tan Andy <tanandy2011@hotmail.com>
2019-06-02 17:03:11 +0800

    Remove Redundant Code

--- SecurityDoorV5.ino
+++ SecurityDoorV5.ino
@@ -53,54 +53,36 @@ Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
 void setup()
 {
-  
-    Serial.begin(9600);
-    Serial.println("@@@@@@@@@@@@@@@@@@@@@1");
+    //Serial.begin(9600);
     SPI.begin();
-    rc.PCD_Init();                //initialize the receiver
-    Serial.println("@@@@@@@@@@@@@@@@@@@@@2");
+    rc.PCD_Init(); //initialize the receiver
     rc.PCD_DumpVersionToSerial(); //show details of card reader module
 
-    Serial.println("@@@@@@@@@@@@@@@@@@@@@");
-    
     finger.begin(57600); //Initiate Fingerprint Scanner
 
-    pinMode(relayPin, OUTPUT);
     pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(ledPin, OUTPUT);
-    pinMode(redPin, OUTPUT);
-    pinMode(buzzerPin, OUTPUT);
 
     keypad.addEventListener(keypadEvent); //add an event listener for this keypad
 
     display.lcdStart();
-    digitalWrite(redPin, HIGH);
-    digitalWrite(relayPin, HIGH);
-    digitalWrite(ledPin, LOW);
+    display.close();
 
-    ///////////////////////////////////////
-    Serial.print("Initialize");
     // see if the card is present and can be initialized:
     if (!SD.begin(chipSelect))
     {
-        Serial.println("Nao ha cartao disponivel");
         // don't do anything more:
         return;
     }
 
-    ///////////////////////////////////////
     Wire.begin();
+    //Initialize the Real Time Clock
     rtc.begin();
-    if (!rtc.begin())
-    {
-        Serial.println("Couldn't find RTC");
-        while (1)
-            ;
-        rtc.adjust(DateTime(2019, 5, 31, 15, 16, 0));
-        // This line sets the RTC with an explicit date & time, for example to set
-        // January 21, 2014 at 3am you would call:
-        // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-    }
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    //rtc.adjust(DateTime(2019, 6, 2, 16, 45, 0));
+
+    nameEnter = "Security Door Rebooted";
+    writeSD(nameEnter);
 }
 
 void loop()
@@ -118,20 +100,16 @@ void loop()
         nameEnter = "Button Override";
         writeSD(nameEnter);
     }
-
-    Serial.println(printKey);
 }
 
 // Taking care of some special events.
 void keypadEvent(KeypadEvent key)
 {
+    //Enter Enroll Fingerprint Mode
     if (fingerPrintMode == 1)
     {
-        Serial.println("Entered fingerprint mode");
-
         while (fingerPrintX >= 0 && fingerPrintX <= 5)
         {
-            Serial.println("Entered fingerprint modemodemodemode");
             Serial.println(fingerPrintX);
             switch (keypad.getState())
             {
@@ -139,26 +117,21 @@ void keypadEvent(KeypadEvent key)
                 fingerPrintX++;
                 sFingerID += key;
                 display.fpNewID(sFingerID);
-
-                Serial.print("Key Entered: ");
-                Serial.println(key);
-                Serial.print("FingerprintX: ");
-                Serial.println(fingerPrintX);
-                Serial.print("String finger ID: ");
-                Serial.println(sFingerID);
+                //Serial.print("Key Entered: ");
+                //Serial.println(key);
+                //Serial.print("FingerprintX: ");
+                //Serial.println(fingerPrintX);
+                //Serial.print("String finger ID: ");
+                //Serial.println(sFingerID);
 
                 switch (key)
                 {
                 case '*':
-                    Serial.println(sFingerID);
                     sFingerID = sFingerID.substring(0, sFingerID.length() - 1);
                     id = sFingerID.toInt();
-                    Serial.println(id);
-                    //delay(5000);
+                    //Serial.println(id);
                     if (id >= 1 && id <= 127)
                     {
-                        Serial.println("INSIDE 1 TO 127 LOOP");
-
                         while (!getFingerprintEnroll())
                             ;
 
@@ -174,23 +147,19 @@ void keypadEvent(KeypadEvent key)
                         fingerPrintX = 10;
                         sFingerID = "";
                     }
-
                     break;
-
                 default:
                     break;
                 }
-
                 break;
-
             default:
                 break;
             }
-
             keypad.getKey();
             delay(100);
         }
     }
+    //Enter the Password
     if (x == 0)
     {
         switch (keypad.getState())
@@ -199,13 +168,12 @@ void keypadEvent(KeypadEvent key)
             x++;
             display.lcdPassword();
             keypad.getKey();
-
             break;
-
         default:
             break;
         }
     }
+    //More than 6 keys entered will automatically be denied
     while (x >= 1 && x <= 6)
     {
 
@@ -228,13 +196,13 @@ void keypadEvent(KeypadEvent key)
             x++;
             pw += "*";
             display.printPassword(pw);
-            Serial.println(key);
+            //Serial.println(key);
             pwEnter += key;
 
             switch (key)
             {
             case '*':
-                Serial.println(pwEnter);
+                //Serial.println(pwEnter);
                 checkPassword();
                 x = 0;
                 break;
@@ -301,58 +269,56 @@ void rfidMode()
     {
         int match = 0;
         int cardID;
-        //this is the part where compare the current tag with pre defined tags
+        //Compare the scanned card with predifined Card
         for (int i = 0; i < N; i++)
         {
-            Serial.print("Testing Against Authorised card no: ");
-            Serial.println(i + 1);
+            //Serial.print("Testing Against Authorised card no: ");
+            //Serial.println(i + 1);
             if (!memcmp(readcard, defcard[i], 4))
             {
+                //Match = 1 If card Found
                 match++;
                 cardID = i;
             }
         }
 
-        if (match) //match ==1
+        if (match)
         {
+            //Card for KON10 BB A7 2E 01
             if (cardID == 0)
             {
-                Serial.println("This is a Kon10");
-                Serial.println("CARD AUTHORISED");
+
                 display.open();
                 display.lcdAuthorised();
                 delay(3000);
-                Serial.println("Turn Off the Lock");
                 readsuccess = 0;
                 nameEnter = "KON10";
                 writeSD(nameEnter);
                 display.close();
             }
+            //Card for Printcess 96 68 47 F4
             else if (cardID == 1)
             {
-                Serial.println("This is a Printcess");
-                Serial.println("CARD AUTHORISED");
                 display.open();
                 display.lcdAuthorised();
                 delay(3000);
-                Serial.println("Turn Off the Lock");
                 readsuccess = 0;
                 nameEnter = "Printcess";
                 writeSD(nameEnter);
                 display.close();
             }
+            //MasterCard D6 BA 45 F4
             else if (cardID == 2)
             {
-                Serial.println("This is a FingerprintCard");
+
                 fingerPrintMode = 1;
                 fingerPrintX = 0;
                 display.fpNewID(sFingerID);
                 keypadEvent(fingerPrintMode);
             }
         }
-        else //match ==0
+        else
         {
-            Serial.println("CARD NOT Authorised");
             display.lcdDenied();
             display.buzzer();
             nameEnter = "Unauthorised CARD";
@@ -385,10 +351,10 @@ int getid()
     for (int i = 0; i < 4; i++)
     {
         readcard[i] = rc.uid.uidByte[i]; //storing the UID of the tag in readcard
-        Serial.print(readcard[i], HEX);
+        //Serial.print(readcard[i], HEX);
     }
-    Serial.println("");
-    Serial.println("Now Comparing with Authorised cards");
+    //Serial.println("");
+    //Serial.println("Now Comparing with Authorised cards");
     rc.PICC_HaltA();
 
     return 1;
@@ -397,62 +363,58 @@ int getid()
 uint8_t getFingerprintID()
 {
     uint8_t p = finger.getImage();
-    Serial.println(p);
     switch (p)
     {
     case FINGERPRINT_OK:
-        Serial.println("Image taken");
+        //Image Taken
         break;
 
     default:
-        Serial.println("Unknown error");
-
+        //Unknown Error
         return p;
     }
 
     // OK success!
 
     p = finger.image2Tz();
-    Serial.println(p);
     switch (p)
     {
     case FINGERPRINT_OK:
-        Serial.println("Image converted");
+        //Image is Converted
         break;
     case FINGERPRINT_IMAGEMESS:
-        Serial.println("Image too messy");
-
+        //Messy Image
         return p;
     default:
-        Serial.println("Unknown error");
+        //Unknown Error
         return p;
     }
 
     // OK converted!
     p = finger.fingerFastSearch();
-    Serial.println(p);
     if (p == FINGERPRINT_OK)
     {
-        Serial.println("Found a print match!");
+        //Found a same match
     }
     else if (p == FINGERPRINT_NOTFOUND)
     {
-        Serial.println("Did not find a match");
+        //Did not find a match
         return p;
     }
     else
     {
-        Serial.println("Unknown error");
-
+        //Unknown Error
         return p;
     }
 
     // found a match!
-    Serial.print("Found ID #");
-    Serial.print(finger.fingerID);
-    Serial.print(" with confidence of ");
-    Serial.println(finger.confidence);
+    //Serial.print("Found ID #");
+    //Serial.print(finger.fingerID);
+    //Serial.print(" with confidence of ");
+    //Serial.println(finger.confidence);
+
     int fingerID = finger.fingerID;
+
     switch (finger.fingerID)
     {
     case 1:
@@ -494,10 +456,10 @@ uint8_t getFingerprintID()
     return finger.fingerID;
 }
 
+//Write to SD card
 void writeSD(String idName)
 {
     File dataFile = SD.open("Log.txt", FILE_WRITE);
-    Serial.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     DateTime now = rtc.now();
     if (dataFile)
     {
@@ -520,9 +482,9 @@ void writeSD(String idName)
     }
     dataFile.close();
 }
+//Enroll new Fingerprint
 uint8_t getFingerprintEnroll()
 {
-
     int p = -1;
     Serial.print("Waiting for valid finger to enroll as #");
     Serial.println(id);
@@ -532,11 +494,9 @@ uint8_t getFingerprintEnroll()
         switch (p)
         {
         case FINGERPRINT_OK:
-            Serial.println("Image taken");
+            //Image Taken
             break;
         case FINGERPRINT_NOFINGER:
-            Serial.println(".");
-            Serial.println("@@@@@@@@@@@@@@@@@@@@@@@@@@!@!@!@@!");
             if (!digitalRead(buttonPin))
             {
                 delay(190);
@@ -546,14 +506,8 @@ uint8_t getFingerprintEnroll()
                 return -1;
             }
             break;
-        case FINGERPRINT_PACKETRECIEVEERR:
-            Serial.println("Communication error");
-            break;
-        case FINGERPRINT_IMAGEFAIL:
-            Serial.println("Imaging error");
-            break;
         default:
-            Serial.println("Unknown error");
+            //Unknown Error
             break;
         }
     }
@@ -564,143 +518,80 @@ uint8_t getFingerprintEnroll()
     switch (p)
     {
     case FINGERPRINT_OK:
-        Serial.println("Image converted");
+        //Image converted
         break;
-    case FINGERPRINT_IMAGEMESS:
-        Serial.println("Image too messy");
-        return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-        Serial.println("Communication error");
-        return p;
-    case FINGERPRINT_FEATUREFAIL:
-        Serial.println("Could not find fingerprint features");
-        return p;
-    case FINGERPRINT_INVALIDIMAGE:
-        Serial.println("Could not find fingerprint features");
-        return p;
+
     default:
-        Serial.println("Unknown error");
+        //Unknown Error
         return p;
     }
-
-    Serial.println("Remove finger");
+    //Remove the Finger 
+    display.removeFinger();
     delay(2000);
+
     p = 0;
     while (p != FINGERPRINT_NOFINGER)
     {
         p = finger.getImage();
     }
-    Serial.print("ID ");
-    Serial.println(id);
     p = -1;
-    Serial.println("Place same finger again");
+
+    //Place fingerprint again
+    display.placeFpAgain();
+
+    //Loop until Fingerprint is Taken For the SecondTime
     while (p != FINGERPRINT_OK)
     {
         p = finger.getImage();
         switch (p)
         {
         case FINGERPRINT_OK:
-            Serial.println("Image taken");
+            //Image Taken
             break;
         case FINGERPRINT_NOFINGER:
-            Serial.print(".");
-            break;
-        case FINGERPRINT_PACKETRECIEVEERR:
-            Serial.println("Communication error");
-            break;
-        case FINGERPRINT_IMAGEFAIL:
-            Serial.println("Imaging error");
             break;
         default:
-            Serial.println("Unknown error");
+            //Unknown Error
             break;
         }
     }
-
     // OK success!
-
     p = finger.image2Tz(2);
     switch (p)
     {
     case FINGERPRINT_OK:
-        Serial.println("Image converted");
-        break;
-    case FINGERPRINT_IMAGEMESS:
-        Serial.println("Image too messy");
-        return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-        Serial.println("Communication error");
-        return p;
-    case FINGERPRINT_FEATUREFAIL:
-        Serial.println("Could not find fingerprint features");
-        return p;
-    case FINGERPRINT_INVALIDIMAGE:
-        Serial.println("Could not find fingerprint features");
-        return p;
+        //Image Converted
+        break;       
     default:
-        Serial.println("Unknown error");
+        //Unknown Error
         return p;
     }
-
     // OK converted!
-    Serial.print("Creating model for #");
-    Serial.println(id);
-
+    // Create Fingerprint Model for ID XXX
     p = finger.createModel();
     if (p == FINGERPRINT_OK)
     {
-        Serial.println("Prints matched!");
-    }
-    else if (p == FINGERPRINT_PACKETRECIEVEERR)
-    {
-        Serial.println("Communication error");
-        return p;
-    }
-    else if (p == FINGERPRINT_ENROLLMISMATCH)
-    {
-        Serial.println("Fingerprints did not match");
-        return p;
+        //Fingerprint Match
     }
     else
     {
-        Serial.println("Unknown error");
+        //Unknown Error
         return p;
     }
-
-    Serial.print("ID ");
-    Serial.println(id);
-    p = finger.storeModel(id);
+    //Stored the Fingerprint
+    p = finger.storeModel(id);  
     if (p == FINGERPRINT_OK)
     {
-        Serial.println("Stored!");
         nameEnter = "Fingerprint Stored ";
-        Serial.println(nameEnter);
         nameEnter += id;
-        Serial.println(nameEnter);
-
         writeSD(nameEnter);
         display.lcdFpStored();
         delay(2000);
         return -1;
     }
-    else if (p == FINGERPRINT_PACKETRECIEVEERR)
-    {
-        Serial.println("Communication error");
-        return p;
-    }
-    else if (p == FINGERPRINT_BADLOCATION)
-    {
-        Serial.println("Could not store in that location");
-        return p;
-    }
-    else if (p == FINGERPRINT_FLASHERR)
-    {
-        Serial.println("Error writing to flash");
-        return p;
-    }
     else
     {
-        Serial.println("Unknown error");
+        //Unknown Error
         return p;
     }
 }
